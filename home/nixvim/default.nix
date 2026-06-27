9c6ae20a4cac27b787c87612258b17f9467a61fd
uvu1 <53385458+uvu1@users.noreply.github.com>
2026-06-27 10:58:22 +0900

    feat(nixos): config nixvim

---
+++ home/nixvim/default.nix
@@ -0,0 +1,37 @@
+{ ... }:
+
+{
+  programs.nixvim = {
+    enable = true;
+
+    globals.mapleader = " ";
+
+    opts = {
+      number = true;
+      relativenumber = true;
+
+      expandtab = true;
+      tabstop = 2;
+      shiftwidth = 2;
+      smartindent = true;
+
+      ignorecase = true;
+      smartcase = true;
+
+      termguicolors = true;
+      signcolumn = "yes";
+      updatetime = 250;
+
+      splitright = true;
+      splitbelow = true;
+    };
+
+    plugins = {
+      gitsigns.enable = true;
+      lualine.enable = true;
+      treesitter.enable = true;
+      web-devicons.enable = true;
+      which-key.enable = true;
+    };
+  };
+}
