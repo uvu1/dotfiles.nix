936989194a9a3f5f5004db59fa82d074959a58e7
uvu1 <53385458+uvu1@users.noreply.github.com>
2026-06-27 10:23:33 +0900

    feat(nixos): add zoxide, starship

--- home/uvu1.nix
+++ home/uvu1.nix
@@ -20,5 +20,25 @@
 
   programs.git.enable = true;
 
+  programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+    };
+
+  programs.staarship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+  programs.zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+
+
   programs.home-manager.enable = true;
 }
